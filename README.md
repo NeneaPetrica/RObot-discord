22be4f32a35d182380076bdc1b5006a0d5ab0b3b
Mircea Ionescu <42213952+mircea35@users.noreply.github.com>
2023-11-04 20:33:58 +0000

    Experimental update: Receiving hugs

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ AstroBot is a small companion for your server. They can do simple things such as
     <li>Roll a dice</li>
     <li>Make a special quotebook for each server they're in</li>
     <li>Say quotes from the server quotebook</li>
-    <li>Have a global economy of hugs</li>
+    <li>Have a global economy of hugs so you can give hugs or gamble them.</li>
 </ul>
 
 
--- bot.py
+++ bot.py
@@ -55,7 +55,12 @@ async def hug(interaction: discord.Interaction, hugged_user: str):
     file = open(f"./bank/{interaction.user.id}.txt", "w")
     file.write(str(temp_int - 1))
     file.close()
-
+    member = discord.utils.get(message.guild.members, name=hugged_user)
+    file = open(f"./bank/{member}.txt", "r")
+    temp_int = int(file.read())
+    file = open(f"./bank/{member}.txt", "w")
+    file.write(str(temp_int + 1))
+    file.close()
     await interaction.response.send_message(f"{interaction.user.mention} hugged {hugged_user}!"
                                             "\n"
                                             "https://imgur.com/gallery/V6HmXVi")
