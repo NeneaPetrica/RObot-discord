607b740f78199203678dfaf08913acf5f89221a5
Mircea Ionescu <42213952+mircea35@users.noreply.github.com>
2024-03-23 19:18:51 +0000

    Testing 2

--- cogs/filtermod.py
+++ cogs/filtermod.py
@@ -8,6 +8,24 @@ class filtermod(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command()
+    async def toggle_filter(self, ctx,, interaction: discord.Interaction):
+            """Toggles the filter setting"""
+            file = open(f"./configs/{interaction.guild.id}-filter.txt", "r")
+            temp_int = int(file.read())
+
+            if temp_int == 0:
+                file = open(f"./configs/{interaction.guild.id}-filter.txt", "w")
+                file.write(str(1))
+                file.close()
+                await ctx.send("Filter is now turned on for this server.")
+            else:
+                file = open(f"./bank/{interaction.guild.id}-filter.txt", "w")
+                file.write(str(0))
+                file.close()
+                await ctx.send("Filter is now turned on for this server.")
+
+
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message, interaction: discord.Interaction):
         if 'https://www.instagram.com/' in message.content.lower():
@@ -18,11 +36,6 @@ class filtermod(commands.Cog):
                 await message.channel.send('FilterMod: Instagram posts are not allowed on this server.')
             else:
                 await print(f"Guild {interaction.guild.id} has filter turned off")
-
-    @commands.command()
-    async def toggle_filter(self, ctx):
-            """Toggles the filter setting"""
-            await discord.Message.channel.send("Test")
 
 async def setup(bot):
     await bot.add_cog(filtermod(bot))
