aba097ffd664e4627db64c743be011344ff2c149
Mircea Ionescu <42213952+mircea35@users.noreply.github.com>
2024-03-23 19:24:59 +0000

    hotfix-3

--- cogs/filtermod.py
+++ cogs/filtermod.py
@@ -2,10 +2,22 @@ import discord
 from discord.ext import commands
 from discord import *
 from discord.ext.commands import has_permissions
+import asyncio
 
 class filtermod(commands.Cog,  name="Basic Commands"):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, ctx, message: discord.Message, interaction: discord.Interaction):
+        if 'https://www.instagram.com/' in message.content.lower():
+            file = open(f"./bank/{interaction.guild.id}.txt", "r")
+            temp_int = int(file.read())
+            if temp_int == 1:
+                await message.delete()
+                await ctx.send('FilterMod: Instagram posts are not allowed on this server.')
+            else:
+                await print(f"Guild {interaction.guild.id} has filter turned off")
 
     @commands.command()
     async def toggle_filter(self, ctx, interaction: discord.Interaction):
@@ -23,18 +35,6 @@ class filtermod(commands.Cog,  name="Basic Commands"):
                 file.write(str(0))
                 file.close()
                 await ctx.send("Filter is now turned on for this server.")
-
-
-    @commands.Cog.listener()
-    async def on_message(self, message: discord.Message, interaction: discord.Interaction):
-        if 'https://www.instagram.com/' in message.content.lower():
-            file = open(f"./bank/{interaction.guild.id}.txt", "r")
-            temp_int = int(file.read())
-            if temp_int == 1:
-                await message.delete()
-                await message.channel.send('FilterMod: Instagram posts are not allowed on this server.')
-            else:
-                await print(f"Guild {interaction.guild.id} has filter turned off")
 
 async def setup(bot):
     await bot.add_cog(filtermod(bot))
