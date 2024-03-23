863319f9198c528702621636e1582d23fd39f192
Mircea Ionescu <42213952+mircea35@users.noreply.github.com>
2024-03-23 22:57:31 +0000

    trying to make it only for admins

--- cogs/filtermod.py
+++ cogs/filtermod.py
@@ -22,9 +22,9 @@ class filtermod(commands.Cog):
                 file.close()
                 await print(f"Guild {message.guild.id} has filter turned off")
 
-    @app_commands.command(name = "togglefilter", description= "What? It works????")
+    @app_commands.command(name = "toggle_filter", description= "Toggles the social media filter")
+    @app_commands.has_permissions(administrator=True)
     async def togglefilter(self, interaction: discord.Interaction):
-
         if os.path.isfile(f"./configs/{interaction.guild.id}-filter.txt") == False:
 
             file = open(f"./configs/{interaction.guild.id}-filter.txt", "w")
