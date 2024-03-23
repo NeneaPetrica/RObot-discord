f7a8b0ec3248169c4a2a36b2037819f4165419de
Mircea Ionescu <42213952+mircea35@users.noreply.github.com>
2024-03-23 22:40:46 +0000

    Bug fixing #5

--- cogs/filtermod.py
+++ cogs/filtermod.py
@@ -24,25 +24,26 @@ class filtermod(commands.Cog):
 
     @app_commands.command(name = "togglefilter", description= "What? It works????")
     async def togglefilter(self, interaction: discord.Interaction):
+        
         if os.path.isfile(f"./configs/{interaction.guild.id}-filter.txt") == False:
             file = open(f"./configs/{interaction.guild.id}-filter.txt", "w")
             file.write(str(1))
             file.close()
             print("Created FilterMod File")
-
-        file = open(f"./configs/{interaction.guild.id}-filter.txt", "r")
-        temp_int = int(file.read())
-
-        if temp_int == 0:
-            file = open(f"./configs/{interaction.guild.id}-filter.txt", "w")
-            file.write(str(1))
-            file.close()
-            await interaction.response.send_message("Filter is now turned on for this server.")
         else:
-            file = open(f"./configs/{interaction.guild.id}-filter.txt", "w")
-            file.write(str(0))
-            file.close()
-            await interaction.response.send_message("Filter is now turned off for this server.")
+            file = open(f"./configs/{interaction.guild.id}-filter.txt", "r")
+            temp_int = int(file.read())
+
+            if temp_int == 0:
+                file = open(f"./configs/{interaction.guild.id}-filter.txt", "w")
+                file.write(str(1))
+                file.close()
+                await interaction.response.send_message("Filter is now turned on for this server.")
+            else:
+                file = open(f"./configs/{interaction.guild.id}-filter.txt", "w")
+                file.write(str(0))
+                file.close()
+                await interaction.response.send_message("Filter is now turned off for this server.")
 
 
 async def setup(bot: commands.Bot) -> None:
