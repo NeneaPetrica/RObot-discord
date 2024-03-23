ec5d43c7142dbcfcb03fb52e1e82d62ffa84e455
Mircea Ionescu <42213952+mircea35@users.noreply.github.com>
2024-03-23 19:55:41 +0000

    added ro-fun

---
+++ cogs/ro-fun.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+
+class distractie(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message, interaction: discord.Interaction):
+        if 'bag pula' in message.content.lower():
+            await message.channel.send('Si eu sa mor!')
+
+        if 'mario e gae' in message.content.lower():
+            await message.channel.send('Cel mai gaee')
+
+        if 'sugi pula' in message.content.lower():
+            await message.channel.send('O sugi tu ma!')
+
+        if 'la multi ani' in message.content.lower():
+            await message.channel.send('ADUCETI BAUTURAAAAAA!!!!')
+
+
+async def setup(bot):
+    await bot.add_cog(distractie(bot))
