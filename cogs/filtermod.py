0716dad996e46b135fd4ac5ebd2216c3fcaa191f
Mircea Ionescu <42213952+mircea35@users.noreply.github.com>
2024-03-23 22:05:05 +0000

    te

--- cogs/filtermod.py
+++ cogs/filtermod.py
@@ -23,7 +23,7 @@ class filtermod(commands.Cog):
         await self.process_commands(message)
 
 
-    @app_commands.command(name = "togglefilter")
+    @app_commands.command(name = "togglefilter", description= "What? It works????")
     async def togglefilter(self, interaction: discord.Interaction):
         file = open(f"./configs/{interaction.guild.id}-filter.txt", "r")
         temp_int = int(file.read())
