c203d88aeb25f3907f9d44f537b603dc1738047a
Mircea Ionescu <42213952+mircea35@users.noreply.github.com>
2024-03-23 20:26:01 +0000

    testing

--- cogs/filtermod.py
+++ cogs/filtermod.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 class filtermod(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        
+
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -27,12 +27,12 @@ class filtermod(commands.Cog):
         temp_int = int(file.read())
 
         if temp_int == 0:
-            file = open(f"./configs/{ctx.guild.id}-filter.txt", "w")
+            file = open(f"../configs/{ctx.guild.id}-filter.txt", "w")
             file.write(str(1))
             file.close()
             await ctx.send("Filter is now turned on for this server.")
         else:
-            file = open(f"./bank/{ctx.guild.id}-filter.txt", "w")
+            file = open(f"../configs/{ctx.guild.id}-filter.txt", "w")
             file.write(str(0))
             file.close()
             await ctx.send("Filter is now turned on for this server.")
