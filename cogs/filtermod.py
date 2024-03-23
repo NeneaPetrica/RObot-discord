fb18b4df397ea6414ecbf7f12a42527e50e93f56
Mircea Ionescu <42213952+mircea35@users.noreply.github.com>
2024-03-23 19:26:45 +0000

    Testing

--- cogs/filtermod.py
+++ cogs/filtermod.py
@@ -9,15 +9,16 @@ class filtermod(commands.Cog, name="Basic Commands"):
         self.bot = bot
 
     @commands.Cog.listener()
-    async def on_message(self, ctx, message: discord.Message, interaction: discord.Interaction):
+    async def on_message(self, message: discord.Message, interaction: discord.Interaction):
         if 'https://www.instagram.com/' in message.content.lower():
             file = open(f"./bank/{interaction.guild.id}.txt", "r")
             temp_int = int(file.read())
             if temp_int == 1:
                 await message.delete()
-                await ctx.send('FilterMod: Instagram posts are not allowed on this server.')
+                await message.channel.send('FilterMod: Instagram posts are not allowed on this server.')
             else:
                 await print(f"Guild {interaction.guild.id} has filter turned off")
+            print("action made")
 
     @commands.command()
     async def toggle_filter(self, ctx, interaction: discord.Interaction):
