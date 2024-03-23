4a97013b2617b8a9d5a3639f00b548819c9b8e9e
Mircea Ionescu <42213952+mircea35@users.noreply.github.com>
2024-03-23 16:37:56 +0000

    Commands

--- filtermod.py
+++ filtermod.py
@@ -9,4 +9,13 @@ class FliterMod_cog(commands.Cog):
         if 'https://www.instagram.com/' in message.content.lower():
             await message.delete()
             await message.channel.send('FilterMod: Instagram posts are not allowed on the server.')
-
+    
+    @commands.command()
+    async def hello(self, ctx, *, member: discord.Member = None):
+        """Says hello"""
+        member = member or ctx.author
+        if self._last_member is None or self._last_member.id != member.id:
+            await ctx.send(f'Hello {member.name}~')
+        else:
+            await ctx.send(f'Hello {member.name}... This feels familiar.')
+        self._last_member = member
