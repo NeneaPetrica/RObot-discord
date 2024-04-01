7e9f45fc6b075414f3110f816ce7e6acd4f7f527
Mircea Ionescu <42213952+mircea35@users.noreply.github.com>
2024-04-01 15:13:03 +0100

    Bug fix #7

--- cogs/police.py
+++ cogs/police.py
@@ -14,9 +14,10 @@ class police(commands.Cog):
 
     @app_commands.command(name = "delete", description="Deletes messages")
     @app_commands.checks.has_permissions(manage_guild = True)
-    async def delete(self, message: discord.Message, messages: int):
+    async def delete(self, interaction: discord.Interaction, message: discord.Message, messages: int):
         for i in messages:
             await message.delete()
+        await interaction.response.send_message(f"Deleted {messages} messages.")
 
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(police(bot))
