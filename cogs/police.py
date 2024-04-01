3ae48017a6337244498e34c139c79344c37d433d
Mircea Ionescu <42213952+mircea35@users.noreply.github.com>
2024-04-01 15:00:50 +0100

    Bug fixing #1

--- cogs/police.py
+++ cogs/police.py
@@ -4,21 +4,13 @@ from discord import app_commands
 import os
 import random
 
-class core(commands.Cog):
+class police(commands.Cog):
     def __init__(self, bot : commands.Bot):
         self.bot = bot
 
     @commands.Cog.listener()
     async def on_ready():
-       await print("Loading core...")
-
-    @app_commands.command(name = "hello", description= "Toggles the social media filter")
-    async def hello(self, interaction: discord.Interaction):
-        await interaction.response.send_message(f"Hi, {interaction.user.mention}!")
-
-    @app_commands.command(name = "dice", description= "Rolls a dice")
-    async def dice(self, interaction: discord.Interaction):
-        await interaction.response.send_message(random.randint(1, 6))
+       await print("Loading police.py...")
 
     @app_commands.command(name = "delete", description= "Deletes messages")
     async def delete(self, selection: 0, interaction: discord.Interaction, message: discord.Message):
@@ -26,4 +18,4 @@ class core(commands.Cog):
             await message.delete()
 
 async def setup(bot: commands.Bot) -> None:
-    await bot.add_cog(core(bot))
+    await bot.add_cog(police(bot))
