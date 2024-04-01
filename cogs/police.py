16d56d5a46dc5c285574158b74cefd7b68003ed0
Mircea Ionescu <42213952+mircea35@users.noreply.github.com>
2024-04-01 15:07:38 +0100

    test #3

--- cogs/police.py
+++ cogs/police.py
@@ -14,8 +14,8 @@ class police(commands.Cog):
 
     @app_commands.command(name = "delete", description="Deletes messages")
     @app_commands.checks.has_permissions(manage_guild = True)
-    async def delete(self, message: discord.Message, selection: None):
-        for i in selection:
+    async def delete(self, message: discord.Message, args):
+        for i in args:
             await message.delete()
 
 async def setup(bot: commands.Bot) -> None:
