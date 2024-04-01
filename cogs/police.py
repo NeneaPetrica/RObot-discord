99e2ce548ce1b24fe28806706da81231da905567
Mircea Ionescu <42213952+mircea35@users.noreply.github.com>
2024-04-01 19:11:17 +0100

    Changed the permission needed for delete command

--- cogs/police.py
+++ cogs/police.py
@@ -12,12 +12,12 @@ class police(commands.Cog):
 
     @app_commands.command(name = "delete", description="Deletes messages")
     async def delete(self, interaction: discord.Interaction, messages: int):
-        if interaction.user.guild_permissions.manage_guild == True:
+        if interaction.user.guild_permissions.manage_messages == True:
             await interaction.response.defer()
             deleted = await interaction.channel.purge(limit=messages, bulk= True)
             await interaction.channel.send(f'Deleted {len(deleted)} message(s)')
         else:
-            await interaction.response.send_message("You don't have the following permission: manage_guild.")
+            await interaction.response.send_message("You don't have the following permission: manage_messages.")
     
 
 
