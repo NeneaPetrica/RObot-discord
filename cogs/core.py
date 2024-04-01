b4e0976bf3fb173c6ea7334904b25135c6c32aba
Mircea Ionescu <42213952+mircea35@users.noreply.github.com>
2024-04-01 16:49:04 +0100

    Testing reload command

--- cogs/core.py
+++ cogs/core.py
@@ -16,8 +16,8 @@ class core(commands.Cog):
     async def hello(self, interaction: discord.Interaction):
         await interaction.response.send_message(f"Hi, {interaction.user.mention}!")
     
-    @app_commands.command(name = "test_rel", description= "Toggles the social media filter")
-    async def test_rel(self, interaction: discord.Interaction):
+    @app_commands.command(name = "reload_test", description= "Toggles the social media filter")
+    async def reload_test(self, interaction: discord.Interaction):
         await interaction.response.send_message(f"Testing rel works!!!, {interaction.user.mention}!")
 
     @app_commands.command(name = "dice", description= "Rolls a dice")
