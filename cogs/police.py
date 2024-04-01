a1de8e795ae08adcb99aba461dd266672a68f366
Mircea Ionescu <42213952+mircea35@users.noreply.github.com>
2024-04-01 15:14:51 +0100

    Trying out #6

--- cogs/police.py
+++ cogs/police.py
@@ -1,8 +1,6 @@
-from discord.ext import commands
 import discord
+from discord.ext import commands
 from discord import app_commands
-import os
-import random
 
 class police(commands.Cog):
     def __init__(self, bot : commands.Bot):
@@ -14,10 +12,10 @@ class police(commands.Cog):
 
     @app_commands.command(name = "delete", description="Deletes messages")
     @app_commands.checks.has_permissions(manage_guild = True)
-    async def delete(self, interaction: discord.Interaction, message: discord.Message, messages: int):
+    async def delete(self, message: discord.Message, messages: int):
         for i in messages:
             await message.delete()
-        await interaction.response.send_message(f"Deleted {messages} messages.")
+        await discord.Interaction.response.send_message(f"Deleted {messages} messages.")
 
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(police(bot))
